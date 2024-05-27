25420930734826ad976cb94e7466ba31cc6db7d5
FernandoBersellini <fernandobersellini@outlook.com.br>
2024-05-27 14:10:41 -0300

    Foi criado a página principal para os usuários do site, com estilização usando CSS

---
+++ inde.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="wrapper-left">
+            <img src="imagens/BBC_logo_black_background-1.webp" alt="">
+            <div class="wrapper-login">
+                <img src="imagens/user.png" alt="">
+                <a href="login.php">Entrar</a>
+            </div>
+        </div>
+    </header>
+    <div class="sub-header">
+        <h1>Bem vindo a BBC.com</h1>
+        <h1>24 de maio de 2024</h1>
+    </div>
+    <main>
+        <div class="wrapper-Main">
+            <div class="mainNews">
+                <h1>Ataques de ursos crescem na área!</h1>
+                <p>Autoridades ainda não sabem o que fazer para resolver essa situação</p>
+            </div>
+            <div class="sub-news-wrapper">
+                <div id="div1" style="background-color:blue">div 1</div>
+                <div id="div2" style="background-color:yellow">div 2</div>
+                <div id="div3" style="background-color:red">div 3</div>
+                <div id="div4" style="background-color: green">div 4</div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
